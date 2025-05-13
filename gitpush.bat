15cdc876dd6c69a939d26db3e831e38eb7c788d8
rweilwww <github@shaw-weil.com>
2025-05-13 12:48:43 -0400

    from indesign 2025-05-13 16:48

---
+++ gitpush.bat
@@ -0,0 +1,13 @@
+
+cd /inetpub/wwwroot/w/panhandle
+
+
+git add .
+git commit -m "from indesign 2025-05-13 16:48" --no-edit
+git push main
+
+rem wait for one minute to allow git hub to get its act together
+timeout 60
+rem go fo it!!
+timeout 5
+exit
