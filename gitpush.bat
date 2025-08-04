f7ef48818182da16a80a7f2b9a24293ef0e370bb
rweilwww <github@shaw-weil.com>
2025-08-04 13:57:21 -0400

    from indesign 2025-08-04 17:56

--- gitpush.bat
+++ gitpush.bat
@@ -3,8 +3,8 @@ cd /inetpub/wwwroot/w/panhandle
 
 
 git add .
-git commit -m "from indesign 2025-05-13 16:57" --no-edit
-git push main
+git commit -m "from indesign 2025-08-04 17:56" --no-edit
+git push --force --set-upstream origin main
 
 rem wait for one minute to allow git hub to get its act together
 timeout 60
